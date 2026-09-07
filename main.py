ca74e26227c51dbde28a3db81b67d85795114c25
Aakash <kashyapaakash880@gmail.com>
2026-09-07 19:34:00 +0530

    Change app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(     "DB_URI", "sqlite:///posts.db")

--- main.py
+++ main.py
@@ -58,8 +58,7 @@ def load_user(user_id):
 class Base(DeclarativeBase):
     pass
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DB_URI"
-) or "sqlite:///posts.db"
+    "DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
